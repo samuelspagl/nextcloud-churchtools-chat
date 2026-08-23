ad81ff338809c310335e79245122b3f7845d4e28
Samuel Spagl <samuel@spagl-media.de>
2026-08-23 18:09:19 +0200

    Register a dedicated ChurchTools Chat settings section

--- lib/Settings/Admin.php
+++ lib/Settings/Admin.php
@@ -17,7 +17,7 @@ final class Admin implements ISettings {
 	}
 
 	public function getSection(): string {
-		return 'additional';
+		return Application::APP_ID;
 	}
 
 	public function getPriority(): int {
---
+++ lib/Settings/AdminSection.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\ChurchToolsChat\Settings;
+
+use OCA\ChurchToolsChat\AppInfo\Application;
+use OCP\IL10N;
+use OCP\IURLGenerator;
+use OCP\Settings\IIconSection;
+
+final class AdminSection implements IIconSection {
+	public function __construct(
+		private readonly IURLGenerator $urlGenerator,
+		private readonly IL10N $l,
+	) {
+	}
+
+	public function getID(): string {
+		return Application::APP_ID;
+	}
+
+	public function getName(): string {
+		return $this->l->t('ChurchTools Chat');
+	}
+
+	public function getPriority(): int {
+		return 50;
+	}
+
+	public function getIcon(): string {
+		return $this->urlGenerator->imagePath(Application::APP_ID, 'app.svg');
+	}
+}
